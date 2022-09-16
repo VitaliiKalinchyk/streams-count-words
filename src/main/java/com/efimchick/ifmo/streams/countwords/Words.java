e9777acafd942fb5598681cddcc3898b9ab92c00
Vitalii Kalinchyk <karlkory@gmail.com>
2022-09-16 12:37:37 +0300

    done

--- src/main/java/com/efimchick/ifmo/streams/countwords/Words.java
+++ src/main/java/com/efimchick/ifmo/streams/countwords/Words.java
@@ -1,12 +1,25 @@
 package com.efimchick.ifmo.streams.countwords;
 
 
-import java.util.List;
+import java.util.*;
+import java.util.function.*;
+import java.util.stream.Collectors;
 
 public class Words {
 
     public String countWords(List<String> lines) {
-
-        return null;
+        return  lines.stream().flatMap(line -> Arrays.stream(
+                                                line.replaceAll("[^a-zA-Zа-яА-Я]", " ")
+                                                     .split("\\s+")))
+                .map(String::toLowerCase)
+                .collect(Collectors.toMap(Function.identity(), e -> 1, Integer::sum))
+                .entrySet()
+                .stream()
+                .filter(((Predicate<Map.Entry<String, Integer>>) entry -> entry.getKey().length() > 3)
+                                                                           .and(entry -> entry.getValue() > 9))
+                .sorted(Comparator.comparingInt((Map.Entry<String, Integer> o) -> o.getValue()).reversed()
+                                  .thenComparing(Map.Entry::getKey))
+                .map(entry -> entry.getKey() + " - " + entry.getValue())
+                .collect(Collectors.joining("\n"));
     }
 }
